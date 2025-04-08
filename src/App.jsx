b233fc6e34b509084868ec60e74d377a1890f681
Enes Yilmaz <enesyilmaz5157@gmail.com>
2025-04-07 22:58:37 -0400

    started with visuals of the country

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div style={{ textAlign: "center", padding: "2rem", fontSize: "2rem" }}>
+        ✅ Firewatch is Running!
+      </div>
+    );
+  }
+  
+  export default App;
+  
